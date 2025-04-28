306adf66b9e49ef4e70b7b3fe3f172ebcca78178
T. Person <t.person@macbookpro.mynet>
2025-04-28 15:39:05 +0100

    2-topics complete

--- app/model.js
+++ app/model.js
@@ -1,5 +1,16 @@
 const db = require("../db/connection")
 const endpointsJson = require("../endpoints.json")
 
+exports.selectTopics = (req, res, next) => {
+    const query = `SELECT * FROM topics;`
+    return db.query(query)
+        .then((result) => {
+
+        if(!result.rows.length) {
+            return Promise.reject({ status: 404, msg: "Not Found"})
+        }
+      return result.rows;
+    });
+};
 
 
